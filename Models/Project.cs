b1e651fe44359bba4f09c46e4aabe75bdffff75f
stasglazkov95 <stas.glazkov95@gmail.com>
2020-08-17 01:16:33 +0300

    Add all colums to table

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace BissnesHour.Models
 {
     public class Project
     {
-        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; }
     }
 }
